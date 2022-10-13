0591ca594f6f2ce37441997a8fc7d373bff75167
MAR <ellen.bjornsdottir@gmail.com>
2022-10-13 22:32:24 +0200

    selection menu added

--- javamagic.js
+++ javamagic.js
@@ -1,15 +1,17 @@
 setInterval(function () {
   let tokyoTimeNow = document.querySelector("#tokyoTime");
-  tokyoTimeNow.innerHTML = moment().tz("Asia/Tokyo").format(`HH:mm:ss`);
+  if (tokyoTimeNow) {
+    tokyoTimeNow.innerHTML = moment().tz("Asia/Tokyo").format(`HH:mm:ss`);
 
-  let tokyoDateNow = document.querySelector("#tokyoDate");
-  tokyoDateNow.innerHTML = moment().tz("Asia/Tokyo").format(`D MMM YYYY`);
+    let tokyoDateNow = document.querySelector("#tokyoDate");
+    tokyoDateNow.innerHTML = moment().tz("Asia/Tokyo").format(`D MMM YYYY`);
 
-  let parisTimeNow = document.querySelector("#parisTime");
-  parisTimeNow.innerHTML = moment().tz("Europe/Paris").format(`HH:mm:ss`);
+    let parisTimeNow = document.querySelector("#parisTime");
+    parisTimeNow.innerHTML = moment().tz("Europe/Paris").format(`HH:mm:ss`);
 
-  let parisDateNow = document.querySelector("#parisDate");
-  parisDateNow.innerHTML = moment().tz("Europe/Paris").format(`D MMM YYYY`);
+    let parisDateNow = document.querySelector("#parisDate");
+    parisDateNow.innerHTML = moment().tz("Europe/Paris").format(`D MMM YYYY`);
+  }
 }, 1000);
 
 //--------dropdown selection
@@ -18,18 +20,18 @@ let updateCity = document.querySelector("#timezoneDropdown");
 updateCity.addEventListener(`change`, updateClock);
 
 function updateClock(event) {
-  if (event.target.value === "newyork") {
-    let updating = document.querySelector("#empty");
-    updating.innerHTML = `<div class="row cities">
-          <div class="col-7">
-            <h2>New York</h2>
-            <div class="date" id="">
-              ${moment().tz("America/New_York").format(`D MMM YYYY`)}
+  let cityTimezone = event.target.value;
+  let cityName = cityTimezone.replace(`_`, ` `).split("/")[1];
+  let updating = document.querySelector("#timezoneContainer");
+  updating.innerHTML = `<div class="row cities">
+          <div class="col-6">
+            <h2>${cityName}</h2>
+            <div class="date">
+              ${moment().tz(event.target.value).format(`D MMM YYYY`)}
             </div>
           </div>
-          <div class="col-5 time" id="">
-            ${moment().tz("America/New_York").format(`HH:mm`)}
+          <div class="col-6 time">
+            ${moment().tz(event.target.value).format(`HH:mm:ss`)}
           </div>
         </div>`;
-  }
 }
